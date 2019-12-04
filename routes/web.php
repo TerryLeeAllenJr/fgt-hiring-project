8b3108708e4aa6154601bdcabf2225fec19bf234
Lee Allen <lee.allen@union.co>
2019-12-04 09:00:15 -0500

    Setup basic web routes and view scaffolding

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BlogController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->get('/admin','AdminController@index')->name('admin');
