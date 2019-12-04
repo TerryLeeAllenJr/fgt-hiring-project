9b3ca40756b6a59dc28815974fcf316b73aae737
Lee Allen <lee.allen@union.co>
2019-12-03 21:13:32 -0500

    Create cagetories table

---
+++ database/migrations/2019_12_04_020434_create_category_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',64)->nullable(false);
+            $table->string('slug',64)->unique()->nullable(false);
+            $table->nestedSet();
+            $table->integer('user_id')->nullable(false);
+            $table->timestamps();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
