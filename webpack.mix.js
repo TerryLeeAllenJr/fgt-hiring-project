454576a29396687afbdf0b2513214e3b8a876935
Lee Allen <lee.allen@union.co>
2019-12-06 16:45:40 -0500

    Remove unused reference to resources/js/lib from webpack.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/blog.js', 'public/js')
    .js('resources/js/admin.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .copyDirectory('resources/js/lib', 'public/js/lib');
+   .sass('resources/sass/app.scss', 'public/css');
